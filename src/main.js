718295c76924f2652c29e30a6ca0f9564703e777
Danush Ramanan <dan.ramanan@gmail.com>
2024-11-07 15:14:58 -0700

    backend

--- src/main.js
+++ src/main.js
@@ -1,18 +1,10 @@
 const { invoke } = window.__TAURI__.core;
 
-let greetInputEl;
-let greetMsgEl;
-
-async function greet() {
+async function style(content, selectionStart, selectionEnd, styleType) {
   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  return await invoke("style", { content: content, selectionStart: selectionStart, selectionEnd: selectionEnd, styleType: styleType });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+
 });
