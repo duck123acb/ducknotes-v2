118fe978311c7451b5e5aad966c32c5399904f18
duck123acb <dan.ramanan@gmail.com>
2024-11-11 22:18:08 -0700

    md base

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,38 @@
+struct Element {
+	element_type: String,
+	content: String,
+}
+impl Element {
+	fn new(element_type: &str, content: &str) -> Self {
+		Element {
+			element_type: element_type.to_string(),
+			content: content.to_string(),
+		}
+	}
+}
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn style(content: String, selection_start: i32, selection_end: i32, style_type: String) -> String {
-  format!("{}", content)
+fn calculate_node(content: String) -> Vec<String> {
+  let mut elements = Vec::new();
+
+  // for character in content
+  // if character is #*x then do header tag
+  // if character is * then italic
+  // if character is ** then bold
+  // if character is > then blockquote
+  // etc. for ol, ul, code, horizontal rules, links, strikethrough, and sub/superscript. might add more later
+
+  // based on the markdown cheat sheet https://www.markdownguide.org/cheat-sheet/
+
+  elements
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
     .plugin(tauri_plugin_shell::init())
-    .invoke_handler(tauri::generate_handler![style])
+    .invoke_handler(tauri::generate_handler![calculate_node])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 const { invoke } = window.__TAURI__.core;
 
-async function style(content, selectionStart, selectionEnd, styleType) {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  return await invoke("style", { content: content, selectionStart: selectionStart, selectionEnd: selectionEnd, styleType: styleType });
+async function calculateNode(content) {
+  let elements = await invoke("calculate_node", { content: content })
 }
 
 window.addEventListener("DOMContentLoaded", () => {
